a9b7af0e7a6b273d9862c16f62c0d03a579623f8
kimuhuNjuguna <aueskinj@gmail.com>
2025-07-14 12:04:42 +0000

    day one

---
+++ Huawei_AI-Cloud/area.py
@@ -0,0 +1,7 @@
+import math
+#while is exit controlled and for is entry controlled
+radius =float(input("Please input the radius "))
+
+area = math.pi * pow(radius, 2)
+
+print("The area is; ", area)
---
+++ Huawei_AI-Cloud/function.py
@@ -0,0 +1,6 @@
+def polynomial(x):
+    return (3*(x**2) - x + 2)
+
+x = int(input("enter x "))
+
+print(polynomial(x))
